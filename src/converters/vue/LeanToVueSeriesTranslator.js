97322a3a0a129138e618c160b38b36aaa60e36a0
Laur Aliste <laur.aliste@williamhill.com>
2019-12-31 15:17:07 +0100

    add LeanToVueSeriesTranslator.js

---
+++ src/converters/vue/LeanToVueSeriesTranslator.js
@@ -0,0 +1,28 @@
+import logger from '../../logs/log.js';
+import { SeriesType } from '../../constants/Global.js';
+
+// TODO: incomplete
+export default function t(leanChartTypeOrIndicator) {
+    switch (leanChartTypeOrIndicator) {
+        case SeriesType.Line:
+            return 'Line';
+        case SeriesType.Candle:
+            return 'Candles';
+        case 1:
+            return 'Scatter';
+        case 3:
+            return 'Bar';
+        case 4:
+            return 'Flag';
+        case 5:
+            return 'StackedArea';
+        case 6:
+            return 'Pie';
+        case 7:
+            return 'Treemap';
+        default:
+            throw new Error(`unsupported LEAN SeriesType ${leanChartTypeOrIndicator}`);
+    }
+
+    // Channel (KC,BB), Range (RSI), Trades, Splines (DMI),  Spline (EMA,SMA), Segment, Candles, Volume, Splitters, LineTool, RangeTool,
+}
